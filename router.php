9ac9425967ac7e3f8f0c6c7fa74ac731e3026eb2
Liam Chapman <liam.chapman@spreadingjam.com>
2013-11-14 11:44:47 +0000

    added some basic documentation in class. Also improved a couple of functions and included optional direct request method, along with magic method __call()

--- router.php
+++ router.php
@@ -1,20 +1,58 @@
 <?php
-
+/**
+ * @package Router
+ * @author Liam Chapman
+ */
 class Router {
 	
-	public $uri, $actions = array(), $call = array();
-	public $sep = '.';
+	/**
+	 * @var String
+	 */
+	public $uri; 
 	
-	public $alt_request_types = array('POST', 'HEAD', 'PUT', 'DELETE', 'TRACE', 'CONNECT'); //default GET
+	/**
+	 * @var Array
+	 */
+	public $actions = array(); 
 	
-	public function __construct($ignore_qs = false) {
-		$this->uri = $ignore_qs ? str_replace('?'. $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']) : $_SERVER['REQUEST_URI'];		
+	/**
+	 * @var Array
+	 */
+	public $call = array();
+	
+	/**
+	 * @var String
+	 */
+	public $sep = '.';	
+	
+	/**
+	 * @var Array
+	 */
+	public $request_types = array('GET','POST', 'HEAD', 'PUT', 'DELETE', 'TRACE', 'CONNECT');
+	
+	/**
+	 * @param $ignore_qs Boolean
+	 * - Optional paramater to ignore quesry string
+	 */
+	public function __construct ($ignore_qs = false) {
+		$this->uri = $ignore_qs ? str_replace('?'. $_SERVER['QUERY_STRING'], '', $_SERVER['REQUEST_URI']) : $_SERVER['REQUEST_URI'];	
 	}
 	
-	public function actions($actions) {
-		$this->actions = $actions;
+	/**
+	 * @param $actions Array
+	 * - Pass through routes as an array, with key as the URL and value as the action
+	 */
+	public function actions ($actions = array()) {
+		if (!empty($actions)) {
+			foreach ($actions as $route => $action) {
+				$this->actions[trim($route)] = $action;
+			}
+		}
 	}
 	
+	/**
+	 * This function processes and sorts our actions to be ready to be executed
+	 */
 	public function process () {
 		$return = array();
 		foreach ($this->actions as $route => $actions) {
@@ -31,6 +69,10 @@ class Router {
 		return $return;
 	}
 	
+	/**
+	 * When ran this function loops through our actions that have been processed and checks if the URL is valid. 
+	 * Returns a 404 if nothing is found.
+	 */
 	public function execute () {
 		$patterns = $this->patterns();
 		foreach ($this->process() as $route => $callback) {
@@ -45,9 +87,9 @@ class Router {
 					$this->call['method'] = $callback['method'];
 					$this->call['params'] = $params;
 				}
-				# Check for request method if set. All methods default with GET so no need to append it
-				if (in_array($_SERVER['REQUEST_METHOD'], $this->alt_request_types)) {
-					$this->call['method'] = $_SERVER['REQUEST_METHOD'].'_'.$this->call['method'];
+				if (in_array($_SERVER['REQUEST_METHOD'], $this->request_types) && isset($this->call['method'])) {					
+					$request_method		  = $_SERVER['REQUEST_METHOD'] != 'GET' ? $_SERVER['REQUEST_METHOD'].'_' : '';
+					$this->call['method'] = $request_method.$this->call['method'];
 				}
 			}	
 		}
@@ -63,6 +105,11 @@ class Router {
 		}		
 	}
 	
+	/**
+	 * Optional Parameter checks for the URL. 
+	 * e.g. /my-uri/:string/:int
+	 * - /my-uri/title/1
+	 */
 	public function patterns () {
 		return array(
 			':string' 	=> '([^\/]+)',
@@ -71,9 +118,40 @@ class Router {
 		);
 	}
 	
-	public function E404 () {
-		header($_ENV['SERVER_PROTOCOL']." 404 Not Found", true, 404);
-		exit('404 Error');
+	/**
+	 * @param $file String
+	 * Default 404 Command returns a default message, but includes option parameter for a file	 
+	 */
+	public function E404 ($file = null) {
+		header('HTTP/1.0 404 Not Found');
+		if (is_null($file)) {
+			exit("<h1>404 Not Found</h1>\nThe page that you have requested could not be found.");
+		} else {
+			include_once($file);
+			exit;
+		}
+	}
+	
+	/**
+	 * @param $route String
+	 * @param $callback Closure / Function
+	 * Direct request instead of using an action array
+	 */
+	public function request ($route, $callback) {
+		if (is_callable($callback)) {
+			$this->actions[trim($route)] = $callback;
+		}
+	}
+	
+	/**
+	 * Magic method to look for direct request methods
+	 * e.g. ->get('/my-uri', function () {}); , ->post('/my-uri', function (){});
+	 */
+	public function __call ($call, $args) {
+		$call = strtoupper($call);
+		if (in_array($call, $this->request_types) && $_SERVER['REQUEST_METHOD'] == $call) {
+			$this->request($args[0], $args[1]);
+		}
 	}
 	
 }
